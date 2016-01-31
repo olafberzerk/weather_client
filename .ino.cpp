29f87f58181be97fa616c6a135200064c1a13bc6
Brady Herder <bradyherder@hotmail.com>
2016-01-30 21:12:57 -0800

    ready to test

--- .ino.cpp
+++ .ino.cpp
@@ -1,11 +1,11 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2016-01-29 22:12:20
+//This file has been generated on 2016-01-30 21:05:46
 
 #include "Arduino.h"
 #include <SPI.h>
-#include "RF24.h"
+#include <RF24.h>
 #include <dht.h>
 #include <Wire.h>
 #include "convert.h"
--- weather_client.ino
+++ weather_client.ino
@@ -1,5 +1,12 @@
+/* requires
+ * DHTlib
+ * RF24-master
+ * SPI
+ * Wire
+*/
+
 #include <SPI.h>
-#include "RF24.h"
+#include <RF24.h>
 
 #include <dht.h>
 #include <Wire.h>
@@ -8,61 +15,85 @@
 #define DHT22_PIN 2 // pin that the sensor is on for uno.  Digital pin 4 on atmega
 
 dht DHT;
-RF24 nrf(7,8);
+RF24 radio(7,8);
 
 byte addresses[][6] = {"1Node","2Node"};
+// 1Node = pipe going to server
+// 2Node = local pipe
 
 void setup()
 {
+	//setup the radio
+	radio.begin();
+	radio.setChannel(100);
 
-	nrf.begin();
 	// Set the PA Level low to prevent power supply related issues since this is a
 	// getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
-	nrf.setPALevel(RF24_PA_LOW);
+	/*
+	   Set Power Amplifier (PA) level to one of four levels: RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH and RF24_PA_MAX
+	   The power levels correspond to the following output levels respectively: NRF24L01: -18dBm, -12dBm,-6dBM, and 0dBm
+	 */
+	radio.setPALevel(RF24_PA_LOW);
 
-	nrf.openWritingPipe(addresses[1]);
-	nrf.openReadingPipe(1,addresses[0]);
+	radio.openWritingPipe(addresses[0]);
+	//radio.openReadingPipe(1,addresses[1]);
+	// just going to write to the server
 
-	nrf.startListening();
-
-
+	radio.startListening();
 }
 
 void loop()
 {
   // READ DATA
 
+  const char error[] = "sensor fail";
   int status = DHT.read22(DHT22_PIN);
   switch (status)
   {
     case DHTLIB_OK:
-    	{
-    		nrf.stopListening();
-        	if (!nrf.write( &DHT.humidity, sizeof(double) )){
+    {
+    		radio.stopListening();
+        	if (!radio.write(&DHT.humidity, sizeof(double) )){
         	         Serial.println(F("failed to send humidity"));
         	       }
-        	if (!nrf.write( &DHT.temperature, sizeof(double) )){
-        	           Serial.println(F("failed to send temp"));
+        	if (!radio.write(&DHT.temperature, sizeof(double) )){
+        	           Serial.println(F("failed to send temperature"));
         	         }
-    	}
+    }
     break;
     case DHTLIB_ERROR_CHECKSUM:
     {
-
+    	radio.stopListening();
+    	        if (!radio.write(&error, sizeof(error) )){
+    	        	        Serial.println(F("sensor fail"));
+    	        }
     }
     break;
     case DHTLIB_ERROR_TIMEOUT:
     {
-
+    	radio.stopListening();
+    	        if (!radio.write(&error, sizeof(error) )){
+    	        	         Serial.println(F("sensor fail"));
+    	        }
     }
     break;
     default:
     {
-
+    	radio.stopListening();
+    	        if (!radio.write(&error, sizeof(error) )){
+    	        	         Serial.println(F("sensor fail"));
+    	        }
     }
     break;
   }
 
-  nrf.startListening();
-  delay(2000);
+  //radio.startListening();
+  //needed?
+
+  //power management routine
+  /*
+  radio.powerDown();
+  delay(20000);
+  radio.powerUp();
+  */
 }
