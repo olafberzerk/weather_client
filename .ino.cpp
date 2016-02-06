5b7803415a74fa73e8f75f6e8e46f600fc6561d6
Brady Herder <bradyherder@hotmail.com>
2016-02-05 21:47:16 -0800

    Signed-off-by: Brady Herder <bradyherder@hotmail.com>

--- .ino.cpp
+++ .ino.cpp
@@ -1,7 +1,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2016-01-30 21:36:02
+//This file has been generated on 2016-02-05 21:46:06
 
 #include "Arduino.h"
 #include <SPI.h>
--- weather_client.ino
+++ weather_client.ino
@@ -19,8 +19,14 @@ byte addresses[][6] = {"1Node","2Node"};
 // 1Node = pipe going to server
 // 2Node = local pipe
 
+int status;
+const char error[] = "sensor fail";
+
 void setup()
 {
+	delay(2000);
+	Serial.begin(115200);
+	Serial.println("Entering setup\n\n");
 	//setup the radio
 	radio.begin();
 	radio.setChannel(100);
@@ -31,26 +37,39 @@ void setup()
 	   Set Power Amplifier (PA) level to one of four levels: RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH and RF24_PA_MAX
 	   The power levels correspond to the following output levels respectively: NRF24L01: -18dBm, -12dBm,-6dBM, and 0dBm
 	 */
-	radio.setPALevel(RF24_PA_LOW);
+	Serial.println("Setting power level\n\n");
+	radio.setPALevel(RF24_PA_MIN);
 
+	Serial.println("Opening write pipe\n\n");
 	radio.openWritingPipe(addresses[0]);
 	//radio.openReadingPipe(1,addresses[1]);
 	// just going to write to the server
 
-	//radio.startListening();
+	Serial.println("Starting to listen\n\n");
+	radio.startListening();
+
 }
 
 void loop()
 {
   // READ DATA
 
-  const char error[] = "sensor fail";
-  int status = DHT.read22(DHT22_PIN);
+  //const char error[] = "sensor fail";
+  //int status = DHT.read22(DHT22_PIN);
+  Serial.println("Getting sensor status\n\n");
+  status = DHT.read22(DHT22_PIN);
+  Serial.println("Got sensor status\n\n");
+
   switch (status)
   {
     case DHTLIB_OK:
     {
-    		radio.stopListening();
+    	Serial.println("Got temp information\n\n");
+    	Serial.println(DHT.humidity, 2);
+    	Serial.println(DHT.temperature, 2);
+
+    	Serial.println("Stopping listening\n\n");
+    	radio.stopListening();
         	if (!radio.write(&DHT.humidity, sizeof(double) )){
         	         Serial.println(F("failed to send humidity"));
         	       }
@@ -84,6 +103,10 @@ void loop()
     }
     break;
   }
+
+  Serial.println("Delaying 3s\n\n");
+  delay(3000);
+  Serial.println("End of loop and start listening\n\n");
 
   //radio.startListening();
   //needed?
