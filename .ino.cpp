a80d2931166079c344c49d16d3b49865272488d1
Brady Herder <bradyherder@hotmail.com>
2016-02-06 20:46:03 -0800

    Signed-off-by: Brady Herder <bradyherder@hotmail.com>

--- .ino.cpp
+++ .ino.cpp
@@ -1,7 +1,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2016-02-05 22:37:41
+//This file has been generated on 2016-02-06 20:41:38
 
 #include "Arduino.h"
 #include <SPI.h>
--- weather_client.ino
+++ weather_client.ino
@@ -26,7 +26,7 @@ void setup()
 {
 	delay(2000);
 	Serial.begin(115200);
-	Serial.println("Entering setup\n\n");
+	Serial.println("Entering setup");
 	//setup the radio
 	radio.begin();
 	radio.setChannel(100);
@@ -37,59 +37,72 @@ void setup()
 	   Set Power Amplifier (PA) level to one of four levels: RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH and RF24_PA_MAX
 	   The power levels correspond to the following output levels respectively: NRF24L01: -18dBm, -12dBm,-6dBM, and 0dBm
 	 */
-	Serial.println("Setting power level\n\n");
+	Serial.println("Setting power level");
 	radio.setPALevel(RF24_PA_MIN);
 
-	Serial.println("Opening write pipe\n\n");
+	Serial.println("Opening write pipe");
 	radio.openWritingPipe(addresses[0]);
 	//radio.openReadingPipe(1,addresses[1]);
 	// just going to write to the server
 
-	Serial.println("Starting to listen\n\n");
+	Serial.println("Starting to listen");
 	radio.startListening();
 
 }
 
 void loop()
 {
-  // READ DATA
+
+	// READ DATA
 
   //const char error[] = "sensor fail";
   //int status = DHT.read22(DHT22_PIN);
-  Serial.println("Getting sensor status\n\n");
+  Serial.println("Getting sensor status");
   status = DHT.read22(DHT22_PIN);
-  Serial.println("Got sensor status\n\n");
+  Serial.println("Got sensor status");
 
   switch (status)
   {
     case DHTLIB_OK:
     {
-    	Serial.println("Got temp information\n\n");
+    	Serial.println("Got temp information");
     	Serial.println(DHT.humidity, 2);
     	Serial.println(DHT.temperature, 2);
 
-    	Serial.println("Stopping listening\n\n");
+    	Serial.println("Stopping listening");
     	radio.stopListening();
-    	Serial.println("Converting temp to string\n\n");
+    	Serial.println("Converting temp to string");
     	String temperature(DHT.temperature,2);
-    	Serial.println("Converting hum to string\n\n");
+    	Serial.println("Converting hum to string");
     	String humidity(DHT.humidity,2);
     	Serial.println(humidity);
     	Serial.println(temperature);
 
-/***********************************
-//update here
-    	temperature.getBytes((&buf, sizeof(buf));
-//update here
-    	temperature.getBytes((&buf, sizeof(buf));
 
-************************************/
+    	// Length (with one extra character for the null terminator)
+    	int temp_len = temperature.length() + 1;
+    	int hum_len = humidity.length() + 1;
 
+    	// Prepare the character array (the buffer)
+    	char temp_array[temp_len];
+    	char hum_array[hum_len];
 
-        	if (!radio.write(&temperature, sizeof(temperature) )){
+    	// Copy it over
+    	temperature.toCharArray(temp_array, temp_len);
+    	humidity.toCharArray(hum_array, hum_len);
+
+    	Serial.println("Converted to char array");
+    	Serial.println(hum_array);
+    	Serial.println(temp_array);
+
+    	//few ways to do it differently maybe?
+    	//dtostrf();  //one way to convert from double to string
+    	//string.getBytes((&buf, sizeof(buf));
+
+        	if (!radio.write(&temp_array, sizeof(temp_array) )){
         	         Serial.println(F("failed to send humidity"));
         	       }
-        	if (!radio.write(&humidity, sizeof(humidity) )){
+        	if (!radio.write(&hum_array, sizeof(hum_array) )){
         	           Serial.println(F("failed to send temperature"));
         	         }
     }
@@ -120,8 +133,8 @@ void loop()
     break;
   }
 
-  Serial.println("Delaying 3s\n\n");
-  delay(3000);
+  Serial.println("Delaying 10s");
+  delay(10000);
   Serial.println("End of loop and start listening\n\n");
 
   //radio.startListening();
